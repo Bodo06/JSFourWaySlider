a2d888c1635ef3d9d8bfa0f8404399ebfac9dd84
Alexander Pinnecke <alexander.pinnecke@googlemail.com>
2013-03-19 16:13:11 +0100

    - switched to container based usage saving

--- jquery.slidecontent.js
+++ jquery.slidecontent.js
@@ -2,23 +2,29 @@ var SlideContentInUse = false;
 
 (function ($) {
 	$.fn.SlideContent = function (element, position, slideParentHeight, speed, easing, callback) {
-		if (SlideContentInUse) {
+		var that = this;
+
+		var SlideContentInUseAttr = 'SlideContentInUse';
+		if (typeof that.data(SlideContentInUseAttr) == 'undefined') {
+			that.data(SlideContentInUseAttr, false);
+		}
+		if (that.data(SlideContentInUseAttr)) {
 			return;
 		}
+		that.data(SlideContentInUseAttr, true);
 
 		callback = callback || function (elem) {
 			$(elem).remove();
 		};
 		slideParentHeight = slideParentHeight || true;
-		var that = this;
 		var speed = speed || 300;
 		var easing = easing || 'swing';
 		element = $(element);
-		var sibling = this.children();
+		var sibling = that.children();
 
 		// Save margins of container in variables
-		var startMarginX = Math.abs(parseInt(this.css('margin-left')));
-		var startMarginY = Math.abs(parseInt(this.css('margin-top')));
+		var startMarginX = Math.abs(parseInt(that.css('margin-left')));
+		var startMarginY = Math.abs(parseInt(that.css('margin-top')));
 
 		/**
 		 * Animates the container (this) to the given height.
@@ -58,9 +64,8 @@ var SlideContentInUse = false;
 		if (0 == this.children().length) {
 			this.append(resetCSSToStart(element.hide().detach()));
 
-			SlideContentInUse = true;
 			element.fadeIn(speed, function () {
-				SlideContentInUse = false;
+				that.data(SlideContentInUseAttr, false);
 				callback(sibling.detach());
 			});
 			animateContainerHeight(element.outerHeight());
@@ -68,6 +73,7 @@ var SlideContentInUse = false;
 
 			// If the given element is already displayed, return.
 		} else if (sibling.is(element)) {
+			that.data(SlideContentInUseAttr, false);
 			return;
 		}
 
@@ -105,9 +111,8 @@ var SlideContentInUse = false;
 
 			this.prepend(element.detach());
 
-			SlideContentInUse = true;
 			$(element).animate({ 'margin-left': startMarginX + 'px' }, speed, easing, function () {
-				SlideContentInUse = false;
+				that.data(SlideContentInUseAttr, false);
 				element.css('float', 'none');
 				callback(resetCSSToStart(sibling.css('float', 'none')).detach());
 			});
@@ -123,9 +128,8 @@ var SlideContentInUse = false;
 			sibling.css('float', 'left');
 			this.append(element.detach());
 
-			SlideContentInUse = true;
 			$(sibling).animate({ 'margin-left': ( startMarginX - width ) + 'px' }, speed, easing, function () {
-				SlideContentInUse = false;
+				that.data(SlideContentInUseAttr, false);
 				callback(resetCSSToStart(sibling.detach()));
 				resetCSSToStart(element.css('float', 'none'));
 			});
@@ -146,9 +150,8 @@ var SlideContentInUse = false;
 
 			this.prepend(element.detach());
 
-			SlideContentInUse = true;
 			$(element).animate({ 'margin-top': startMarginY + 'px' }, speed, easing, function () {
-				SlideContentInUse = false;
+				that.data(SlideContentInUseAttr, false);
 				callback(resetCSSToStart(sibling.detach()));
 			});
 
@@ -160,18 +163,17 @@ var SlideContentInUse = false;
 			});
 			this.append(element.detach());
 
-			SlideContentInUse = true;
 			$(sibling).animate({ 'margin-top': ( startMarginY - parseInt(sibling.outerHeight()) ) + 'px' }, speed, easing, function () {
-				SlideContentInUse = false;
+				that.data(SlideContentInUseAttr, false);
 				callback(resetCSSToStart(sibling.detach()));
 				resetCSSToStart(element);
 			});
 		} else {
-			SlideContentInUse = false;
 			return;
 		}
 
 		tempdir.remove();
 		animateContainerHeight(element.outerHeight());
+		that.data(SlideContentInUseAttr, false);
 	};
 })(jQuery);
